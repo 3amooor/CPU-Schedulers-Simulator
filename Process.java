5a17e11872e2c690653d3e30c2dc749b97e8389a
Omar Tarek Moustafa <tarekomar480@gmail.com>
2025-12-12 13:30:30 +0200

    Initial Commit

---
+++ Process.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Process {
+    String name;
+    int arrivalTime;
+    int burstTime;
+    int remainingTime;
+    int priority;
+    int completionTime;
+    int waitingTime;
+    int turnaroundTime;
+
+    public Process(String name, int arrivalTime, int burstTime, int priority) {
+        this.name = name;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.remainingTime = burstTime;
+        this.priority = priority;
+        this.completionTime = 0;
+        this.waitingTime = 0;
+        this.turnaroundTime = 0;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
